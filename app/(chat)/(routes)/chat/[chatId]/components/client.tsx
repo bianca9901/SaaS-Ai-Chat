713077071be01160d80f2a2866d629d042108a10
bianca9901 <biancawikberg99@yahoo.com>
2024-03-13 08:29:32 +0100

    Create client component for displaying chat details

---
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import ChatHeader from "@/components/ui/chat-header";
+import { Companion, Message } from "@prisma/client";
+import React from "react";
+
+interface ChatClientProps {
+  companion: Companion & {
+    messages: Message[];
+    _count: {
+      messages: number;
+    };
+  };
+}
+
+function ChatClient({
+    companion
+}: ChatClientProps) {
+  return <div className="flex flex-col h-full p-4 space-y-2">
+    <ChatHeader companion={companion}/>
+  </div>;
+}
+
+export default ChatClient;
