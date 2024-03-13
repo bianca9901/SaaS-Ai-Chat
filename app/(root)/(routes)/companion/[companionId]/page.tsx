d21ef48a640dcde1dc035cc208ff34a1c89eb20b
bianca9901 <biancawikberg99@yahoo.com>
2024-03-13 08:44:58 +0100

    Add security measure to restrict editing of other users companions

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import React from "react";
 import CompanionForm from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
   params: {
@@ -9,20 +10,24 @@ interface CompanionIdPageProps {
 }
 
 async function CompanionIdPage({ params }: CompanionIdPageProps) {
+  const { userId } = auth();
   // Check subscription later..
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
 
   const companion = await prismadb.companion.findUnique({
     // attempt to fetch companion based on id from current url
     where: {
       id: params.companionId,
+      userId,
     },
   });
 
-  const categories = await prismadb.category.findMany()
+  const categories = await prismadb.category.findMany();
 
-  return (
-    <CompanionForm initialData={companion} categories={categories}/>
-  )
+  return <CompanionForm initialData={companion} categories={categories} />;
 }
 
 export default CompanionIdPage;
