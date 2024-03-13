5361b11f9e63061bac89b990a3738669bac3bf5a
bianca9901 <biancawikberg99@yahoo.com>
2024-03-13 06:55:23 +0100

    Fix misstake inside href

--- components/ui/companions.tsx
+++ components/ui/companions.tsx
@@ -36,7 +36,7 @@ function Companions({
     {data.map((item) => (
         <Card key={item.id} className="bg-primary/10 rounded-xl cursor-pointer
         hover:opacity-75 transition border-0">
-            <Link href={`/chat${item.id}`}>
+            <Link href={`/chat/${item.id}`}>
                 <CardHeader className="flex items-center justify-center
                 text-center text-muted-foreground">
                     <div className="relative w-32 h-32">
