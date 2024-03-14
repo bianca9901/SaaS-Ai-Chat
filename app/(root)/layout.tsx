7d5c023463eae4e01d704fd562dc78353752bb19
bianca9901 <biancawikberg99@yahoo.com>
2024-03-14 16:32:48 +0100

    Add isPro prop to NavBar and SideBar and check subscription

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,18 @@
 import NavBar from "@/components/ui/navbar"
 import SideBar from "@/components/ui/sidebar";
+import { checkSubscription } from "@/lib/subscription";
 
-function RootLayout({
+async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isPro = await checkSubscription()
   return (
     <div className="h-full">
-        <NavBar/>
+        <NavBar isPro={isPro}/>
         <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
-          <SideBar />
+          <SideBar isPro={isPro} />
         </div>
       <main className="md:pl-20 pt-16 h-full">{children}</main>
     </div>
