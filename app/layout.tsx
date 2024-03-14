31bd48491a4b5bb1b047b880e330377bddee29b3
bianca9901 <biancawikberg99@yahoo.com>
2024-03-14 16:32:07 +0100

    Add ProModal to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
+import ProModal from "@/components/ui/pro-modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ProModal/>
             {children}
             <Toaster/>
           </ThemeProvider>
